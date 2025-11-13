650737fa5901447860e8f7782bbd63688aabef21
bhatgsushant <96242919+bhatgsushant@users.noreply.github.com>
2025-11-13 23:54:07 +0000

    Initial Commit

---
+++ owlitiOS/owlitiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  owlitiOSApp.swift
+//  owlitiOS
+//
+//  Created by Sushant Bhat on 13/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct owlitiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ owlitiOSTests/owlitiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  owlitiOSTests.swift
+//  owlitiOSTests
+//
+//  Created by Sushant Bhat on 13/11/2025.
+//
+
+import Testing
+@testable import owlitiOS
+
+struct owlitiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
